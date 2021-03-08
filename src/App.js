b3834da416538f518e57634d6d29a377735da829
Chuong <chuong.git@gmail.com>
2021-03-08 15:32:11 +1100

    ADD: current status

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import ReactMapGL, {Marker, Popup, Layer, Source} from "react-map-gl";
+import * as parkDate from "./data/skateboard-parks.json";
+import airports from "./data/geoairports.json";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faPaperPlane} from '@fortawesome/free-regular-svg-icons';
+
+const layerStyle = {
+  id: 'point',
+  type: 'circle',
+  paint: {
+    'circle-radius': 5,
+    'circle-color': '#007cbf'
+  }
+};
 
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 16.4667,
+    longitude: 107.6,
+    width: '100vw',
+    height: '100vh',
+    zoom: 4,
+  });
+
+  const [selectedPark, setSelectedPark] = useState(null);
+
+  useEffect(() => {
+    const listener = (e) => {
+      if (e.key === "Escape") {
+        setSelectedPark(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ReactMapGL 
+        {...viewport} 
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/chgsuphub1/ckfpfj4g1099j1atj22h1n46q"
+        onViewportChange={viewport=> {
+          setViewport(viewport)
+        }}
+      >
+        <Source id="airport_loc" type="geojson" data={airports}>
+          <Layer {...layerStyle}/> 
+        </Source>
+        {/* {airports.map(airport => (
+          <Marker key={airport.objectID}
+            latitude={airport._geoloc.lat}
+            longitude={airport._geoloc.lng}
+          >
+            <button>
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </button>
+          </Marker>
+        ))} */}
+
+        {/* {selectedPark && (
+          <Popup
+          latitude={selectedPark.geometry.coordinates[1]}
+          longitude={selectedPark.geometry.coordinates[0]}
+          onClose={() => {
+            setSelectedPark(null)
+          }}
+          >
+            <div>
+              <h2>{selectedPark.properties.NAME}</h2>
+              <p>{selectedPark.properties.DESCRIPTIO}</p>
+            </div>
+          </Popup>
+        )} */}
+      </ReactMapGL>
     </div>
   );
 }
